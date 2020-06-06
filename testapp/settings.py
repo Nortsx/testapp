3197329707566318a834f4c722cc6b52269af0a8
Всеволод Севостьянов <gather@list.ru>
2020-06-06 14:23:32 +0200

    Fixed settings

--- testapp/settings.py
+++ testapp/settings.py
@@ -121,3 +121,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+django_heroku.settings(locals())
